5ce7d86c1b5f2b648eb1806378aa27cdf4e04e71
nicolb2305 <nico@dsl.no>
2023-12-02 00:48:56 +0100

    Refactored day 1 such that both parts share code

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,59 +1,65 @@
+use crate::parse::from_digit;
 use color_eyre::{eyre::ContextCompat, Result};
-use parse::parse;
+use parse::{from_string_and_digit, parse, Parse};
 
 mod parse {
     use nom::{
         branch::alt,
         bytes::complete::{tag, take},
         combinator::map,
+        error::{Error, ErrorKind, ParseError},
         multi::many1,
-        IResult,
+        Err, IResult, Parser,
     };
 
-    fn number_from_string(i: &str) -> IResult<&str, u32> {
-        let parse_num =
-            |num_str, num_name, num_int| map(alt((tag(num_str), tag(num_name))), move |_| num_int);
+    pub trait Parse<'a, O>: Parser<&'a str, O, Error<&'a str>> {}
+    impl<'a, O, T> Parse<'a, O> for T where T: Parser<&'a str, O, Error<&'a str>> {}
+
+    pub fn from_string_and_digit(i: &str) -> IResult<&str, u32> {
+        let num = |num_name, num_int| map(tag(num_name), move |_| num_int);
+
         let (_, num) = alt((
-            parse_num("1", "one", 1),
-            parse_num("2", "two", 2),
-            parse_num("3", "three", 3),
-            parse_num("4", "four", 4),
-            parse_num("5", "five", 5),
-            parse_num("6", "six", 6),
-            parse_num("7", "seven", 7),
-            parse_num("8", "eight", 8),
-            parse_num("9", "nine", 9),
+            from_digit,
+            alt((
+                num("one", 1),
+                num("two", 2),
+                num("three", 3),
+                num("four", 4),
+                num("five", 5),
+                num("six", 6),
+                num("seven", 7),
+                num("eight", 8),
+                num("nine", 9),
+            )),
         ))(i)?;
 
         let (i, _) = take(1usize)(i)?;
         Ok((i, num))
     }
 
-    fn any(i: &str) -> IResult<&str, Option<u32>> {
-        alt((map(number_from_string, Some), map(take(1usize), |_| None)))(i)
+    pub fn from_digit(i: &str) -> IResult<&str, u32> {
+        let (i, num) = take(1usize)(i)?;
+        num.parse()
+            .map(|v| (i, v))
+            .map_err(|_| Err::Error(Error::from_error_kind(i, ErrorKind::IsNot)))
     }
 
-    pub fn parse(i: &str) -> Option<Vec<u32>> {
-        let (_, nums) = many1(any)(i).ok()?;
-        Some(nums.into_iter().flatten().collect())
+    pub fn parse<'a>(
+        parser: impl Parse<'a, u32> + Copy,
+    ) -> impl FnMut(&'a str) -> Option<Vec<u32>> {
+        move |input| {
+            let any =
+                |parser| move |input| alt((map(parser, Some), map(take(1usize), |_| None)))(input);
+            let (_, nums) = many1(any(parser))(input).ok()?;
+            Some(nums.into_iter().flatten().collect())
+        }
     }
 }
 
-fn part_one(input: &str) -> Option<u32> {
+fn calibrate<'a>(input: &'a str, parser: impl Parse<'a, u32> + Copy) -> Option<u32> {
     input
         .lines()
-        .map(|l| {
-            let first = l.chars().find_map(|c| c.to_digit(10))?;
-            let last = l.chars().rev().find_map(|c| c.to_digit(10))?;
-            Some(first * 10 + last)
-        })
-        .sum()
-}
-
-fn part_two(input: &str) -> Option<u32> {
-    input
-        .lines()
-        .map(parse)
+        .map(parse(parser))
         .map(|res| {
             res.and_then(|list| {
                 let first = list.first()?;
@@ -67,10 +73,10 @@ fn part_two(input: &str) -> Option<u32> {
 fn main() -> Result<()> {
     let input = include_str!("../../input/day1.txt");
 
-    let calibration_value1 = part_one(input).wrap_err("no digits found")?;
+    let calibration_value1 = calibrate(input, from_digit).wrap_err("no digits found")?;
     println!("First calibration value: {calibration_value1}");
 
-    let calibration_value2 = part_two(input).wrap_err("no digits found")?;
+    let calibration_value2 = calibrate(input, from_string_and_digit).wrap_err("no digits found")?;
     println!("Second calibration value: {calibration_value2}");
     Ok(())
 }
