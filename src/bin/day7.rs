2d0b1b7ae19826a07aa0cd219d3a257ecddd34d5
nicolb2305 <nico@dsl.no>
2023-12-07 19:26:39 +0100

    Rewrite of day 7

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,5 +1,5 @@
 use parse::parse;
-use std::{cmp::Ordering, str::FromStr};
+use std::{cmp::Ordering, collections::HashMap, str::FromStr};
 use strum::{EnumIter, IntoEnumIterator};
 use thiserror::Error;
 
@@ -12,8 +12,7 @@ pub enum Error {
 }
 
 pub type DayResult<T> = Result<T, Error>;
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, EnumIter, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, EnumIter, Clone, Copy, Hash)]
 pub enum Card {
     Two,
     Three,
@@ -31,12 +30,17 @@ pub enum Card {
 }
 
 impl Card {
-    fn joker_cmp(self, other: Self) -> Ordering {
-        match (self, other) {
-            (Self::J, Self::J) => Ordering::Equal,
-            (Self::J, _) => Ordering::Less,
-            (_, Self::J) => Ordering::Greater,
-            (x, y) => x.cmp(&y),
+    fn cmp(self, other: Self, no_joker: bool) -> Ordering {
+        let compare = (self as u32).cmp(&(other as u32));
+        if no_joker {
+            compare
+        } else {
+            match (self, other) {
+                (Self::J, Self::J) => Ordering::Equal,
+                (Self::J, _) => Ordering::Less,
+                (_, Self::J) => Ordering::Greater,
+                _ => compare,
+            }
         }
     }
 }
@@ -75,256 +79,70 @@ pub enum HandType {
     FiveOfAKind,
 }
 
-pub trait Hand {
-    fn number_of_a_kind_with_exclusion(&self, num: usize, exclude: Option<Card>) -> Option<Card>;
-    fn number_of_a_kind(&self, num: usize) -> Option<Card> {
-        self.number_of_a_kind_with_exclusion(num, None)
-    }
-    fn full_house(&self) -> Option<HandType>;
-    fn two_pair(&self) -> Option<HandType>;
-    fn hand_type(&self) -> HandType;
-    fn bid(&self) -> u64;
-}
-
 #[derive(Debug, PartialEq, Eq)]
-pub struct NoJokerHand {
+pub struct Hand {
     cards: Vec<Card>,
     bid: u64,
 }
 
-impl Hand for NoJokerHand {
-    fn number_of_a_kind_with_exclusion(&self, num: usize, exclude: Option<Card>) -> Option<Card> {
-        Card::iter()
-            .filter(|&card| {
-                if let Some(exclude) = exclude {
-                    card != exclude
-                } else {
-                    true
-                }
-            })
-            .find(|card_type| self.cards.iter().filter(|&card| card == card_type).count() == num)
-    }
-
-    fn full_house(&self) -> Option<HandType> {
-        self.number_of_a_kind(3)
-            .and_then(|_| self.number_of_a_kind(2))
-            .map(|_| HandType::FullHouse)
-    }
-
-    fn two_pair(&self) -> Option<HandType> {
-        self.number_of_a_kind(2)
-            .and_then(|card1| self.number_of_a_kind_with_exclusion(2, Some(card1)))
-            .map(|_| HandType::TwoPair)
-    }
-
-    fn hand_type(&self) -> HandType {
-        if self.number_of_a_kind(5).is_some() {
-            return HandType::FiveOfAKind;
+impl Hand {
+    fn hand_type(&self, no_joker: bool) -> HandType {
+        let mut map: HashMap<_, _> = Card::iter().map(|card| (card, 0)).collect();
+        for card in &self.cards {
+            *map.get_mut(card).unwrap() += 1;
         }
-
-        if self.number_of_a_kind(4).is_some() {
-            return HandType::FourOfAKind;
-        }
-
-        if let Some(hand_type) = self.full_house() {
-            return hand_type;
-        }
-
-        if self.number_of_a_kind(3).is_some() {
-            return HandType::ThreeOfAKind;
-        }
-
-        if let Some(hand_type) = self.two_pair() {
-            return hand_type;
-        }
-
-        if self.number_of_a_kind(2).is_some() {
-            return HandType::OnePair;
-        }
-
-        HandType::HighCard
-    }
-
-    fn bid(&self) -> u64 {
-        self.bid
-    }
-}
-
-impl PartialOrd for NoJokerHand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for NoJokerHand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let order = self.hand_type().cmp(&other.hand_type());
-        if Ordering::Equal == order {
-            self.cards
-                .iter()
-                .zip(other.cards.iter())
-                .find_map(|(x, y)| match x.cmp(y) {
-                    Ordering::Equal => None,
-                    x => Some(x),
-                })
-                .unwrap_or(Ordering::Equal)
+        let j_count = if no_joker {
+            0
         } else {
-            order
+            map.remove(&Card::J).unwrap_or(0)
+        };
+        let max_card = *map.iter().max_by_key(|(_, &i)| i).unwrap().0;
+        let max_count = map.remove(&max_card).unwrap_or(0);
+        let second_max_count = map.values().max().copied().unwrap_or(0);
+
+        match (max_count + j_count, second_max_count) {
+            (5, _) => HandType::FiveOfAKind,
+            (4, _) => HandType::FourOfAKind,
+            (3, 2) => HandType::FullHouse,
+            (3, _) => HandType::ThreeOfAKind,
+            (2, 2) => HandType::TwoPair,
+            (2, _) => HandType::OnePair,
+            _ => HandType::HighCard,
         }
     }
-}
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct JokerHand {
-    cards: Vec<Card>,
-    bid: u64,
-}
-
-impl Hand for JokerHand {
-    fn number_of_a_kind_with_exclusion(&self, num: usize, exclude: Option<Card>) -> Option<Card> {
-        Card::iter()
-            .filter(|&card| {
-                if let Some(exclude) = exclude {
-                    card != exclude
-                } else {
-                    true
-                }
-            })
-            .find(|card_type| {
+    fn cmp(&self, other: &Self, no_joker: bool) -> Ordering {
+        self.hand_type(no_joker)
+            .cmp(&other.hand_type(no_joker))
+            .then_with(|| {
                 self.cards
                     .iter()
-                    .filter(|&card| card == card_type || card == &Card::J)
-                    .count()
-                    == num
+                    .zip(other.cards.iter())
+                    .fold(Ordering::Equal, |order, (x, &y)| {
+                        order.then_with(|| x.cmp(y, no_joker))
+                    })
             })
     }
-
-    fn full_house(&self) -> Option<HandType> {
-        let three_of_a_kind = Card::iter().find_map(|card_type| {
-            let locs: Vec<_> = self
-                .cards
-                .iter()
-                .enumerate()
-                .filter_map(|(i, card)| {
-                    if card == &card_type || card == &Card::J {
-                        Some(i)
-                    } else {
-                        None
-                    }
-                })
-                .collect();
-            if locs.len() >= 3 {
-                Some(locs)
-            } else {
-                None
-            }
-        })?;
-        // let card_type = self.cards[three_of_a_kind[0]];
-        let _ = Card::iter().find(|&card_type| {
-            self.cards
-                .iter()
-                .enumerate()
-                .filter(|(i, &card)| {
-                    !three_of_a_kind.contains(i) && (card == card_type || card == Card::J)
-                })
-                .count()
-                == 2
-        })?;
-        Some(HandType::FullHouse)
-    }
-
-    fn two_pair(&self) -> Option<HandType> {
-        let two_of_a_kind = Card::iter().find_map(|card_type| {
-            let locs: Vec<_> = self
-                .cards
-                .iter()
-                .enumerate()
-                .filter_map(|(i, card)| {
-                    if card == &card_type || card == &Card::J {
-                        Some(i)
-                    } else {
-                        None
-                    }
-                })
-                .collect();
-            if locs.len() >= 2 {
-                Some(locs)
-            } else {
-                None
-            }
-        })?;
-        let _ = Card::iter().find(|&card_type| {
-            self.cards
-                .iter()
-                .enumerate()
-                .filter(|(i, &card)| {
-                    !two_of_a_kind.contains(i) && (card == card_type || card == Card::J)
-                })
-                .count()
-                == 2
-        })?;
-        Some(HandType::TwoPair)
-    }
-
-    fn hand_type(&self) -> HandType {
-        if self.number_of_a_kind(5).is_some() {
-            return HandType::FiveOfAKind;
-        }
-
-        if self.number_of_a_kind(4).is_some() {
-            return HandType::FourOfAKind;
-        }
-
-        if let Some(hand_type) = self.full_house() {
-            return hand_type;
-        }
-
-        if self.number_of_a_kind(3).is_some() {
-            return HandType::ThreeOfAKind;
-        }
-
-        if let Some(hand_type) = self.two_pair() {
-            return hand_type;
-        }
-
-        if self.number_of_a_kind(2).is_some() {
-            return HandType::OnePair;
-        }
-
-        HandType::HighCard
-    }
-
-    fn bid(&self) -> u64 {
-        self.bid
-    }
 }
 
-impl PartialOrd for JokerHand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
+#[derive(Debug)]
+pub struct Game {
+    hands: Vec<Hand>,
 }
 
-impl Ord for JokerHand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let order = self.hand_type().cmp(&other.hand_type());
-        if Ordering::Equal == order {
-            self.cards
-                .iter()
-                .zip(other.cards.iter())
-                .find_map(|(x, y)| match x.joker_cmp(*y) {
-                    Ordering::Equal => None,
-                    x => Some(x),
-                })
-                .unwrap_or(Ordering::Equal)
-        } else {
-            order
-        }
+impl Game {
+    fn winnings(&mut self, no_joker: bool) -> u64 {
+        self.hands.sort_by(|x, y| x.cmp(y, no_joker));
+        self.hands
+            .iter()
+            .enumerate()
+            .map(|(i, hand)| (i as u64 + 1) * hand.bid)
+            .sum()
     }
 }
 
 mod parse {
-    use crate::{Card, DayResult, Error, Game, NoJokerHand};
+    use crate::{Card, DayResult, Error, Game, Hand};
     use std::str::FromStr;
     use winnow::{
         ascii::{dec_uint, multispace1},
@@ -337,62 +155,32 @@ mod parse {
         take(1usize).try_map(Card::from_str).parse_next(input)
     }
 
-    fn hand(input: &mut &str) -> PResult<NoJokerHand> {
+    fn hand(input: &mut &str) -> PResult<Hand> {
         let (cards, _, bid) = (repeat(1.., card), multispace1, dec_uint).parse_next(input)?;
-        Ok(NoJokerHand { cards, bid })
+        Ok(Hand { cards, bid })
     }
 
-    fn game(input: &mut &str) -> PResult<Game<NoJokerHand>> {
+    fn game(input: &mut &str) -> PResult<Game> {
         let hands = separated(1.., hand, multispace1).parse_next(input)?;
         Ok(Game { hands })
     }
 
-    pub fn parse(input: &str) -> DayResult<Game<NoJokerHand>> {
+    pub fn parse(input: &str) -> DayResult<Game> {
         game.parse(input)
             .map_err(|e| Error::LineParsingError(e.to_string()))
-    }
-}
-
-#[derive(Debug)]
-pub struct Game<H> {
-    hands: Vec<H>,
-}
-
-impl<H: Hand + Ord> Game<H> {
-    fn winnings(&mut self) -> u64 {
-        self.hands.sort_unstable();
-        self.hands
-            .iter()
-            .enumerate()
-            .map(|(i, hand)| (i as u64 + 1) * hand.bid())
-            .sum()
-    }
-}
-
-impl Game<NoJokerHand> {
-    fn convert(self) -> Game<JokerHand> {
-        let hands = self
-            .hands
-            .into_iter()
-            .map(|hand| JokerHand {
-                cards: hand.cards,
-                bid: hand.bid,
-            })
-            .collect();
-        Game { hands }
     }
 }
 
 fn main() -> DayResult<()> {
     let input = include_str!("../../input/day7.txt");
     let mut game = parse(input)?;
-    let winnings = game.winnings();
+    let winnings = game.winnings(true);
     println!("Winnings: {winnings}");
-    // assert_eq!(winnings, 253_910_319);
+    assert_eq!(winnings, 253_910_319);
 
-    let mut game2 = game.convert();
-    let winnings_with_joker = game2.winnings();
+    let winnings_with_joker = game.winnings(false);
     println!("Winnings with joker: {winnings_with_joker}");
-    // assert_eq!(winnings_with_joker, 254_083_736);
+    assert_eq!(winnings_with_joker, 254_083_736);
+
     Ok(())
 }
